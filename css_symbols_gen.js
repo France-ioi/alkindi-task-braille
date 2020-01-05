49f53ce6d94720860ca2773fc5b988d9b76b27a4
Chirantha Dulanjala <k.a.c.dulanjala@gmail.com>
2020-01-05 09:41:35 +0000

    added symbols css generator

---
+++ css_symbols_gen.js
@@ -0,0 +1,52 @@
+var fs = require('fs');
+var file = './src/symbols.css';
+
+try {
+    fs.unlinkSync(file);
+} catch (err) { } // eslint-disable-line
+
+var stream = fs.createWriteStream(file);
+
+function range (til) {
+    var x = 0, xs = [];
+    while (x < til) xs.push(x++);
+    return xs;
+}
+
+function generateBitmasks (n) {
+    return range(Math.pow(2, n)).map(function (x) {
+        return x.toString(2);
+    });
+}
+
+const data = generateBitmasks(12);
+
+stream.once('open', function () {
+    let c = 0;
+    for (let k = 1; k < data.length; k++) {
+        const numStr = data[k];
+        const dots = [];
+        const indexStart = 12 - numStr.length;
+        for (let i = 0; i < numStr.length; i++) {
+            if (numStr[i] === '1') {
+                dots.push(indexStart + i);
+            }
+        }
+
+        ['a', 'b', 'c'].forEach((l, i) => {
+            const sym = `.sy${k}${l}`;
+            dots.forEach(d => stream.write(`${sym} .d${i}_${d},`));
+        });
+
+        // if (k === 5) {
+        //     break;
+        // }
+        if (k > c + 250) {
+            c = k;
+            console.log('written upto symbol', k);
+            stream.write(`.z{fill: #000;}`);
+        }
+    }
+    stream.write(` z {fill: #000;}`);
+    stream.end();
+});
